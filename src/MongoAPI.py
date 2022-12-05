d3e7493fd7780a5189a39ed2ad99a293ef9bfa78
Juan Tur <jtur@cifpfbmoll.eu>
2022-12-05 23:08:33 +0100

    feature-juan(MongoAPI.py): Crea un nuevo documento json donde guarda todos los documentos de la base de datos

--- src/MongoAPI.py
+++ src/MongoAPI.py
@@ -1,21 +1,30 @@
+# Importamos librerias necesarias 
 import requests
 import json
-url = "https://data.mongodb-api.com/app/data-aeqmn/endpoint/data/v1/action/find"
 
-payload = json.dumps({
-    "collection": "bikes",
-    "database": "bike_database",
-    "dataSource": "bike",
-    "filter": {
-        
-    }
-})
-headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Request-Headers': '*',
-  'api-key': "kX4cSFDNtPfXvCxbpXUnKpiQppEdI7Uc8lfD4LjgEcF6WLDJ8YEAcP0IqiSGlhFu", 
-}
+# Acceder a la base de datos desde la API de Mongo
+def ObtenerJson():
+  url = "https://data.mongodb-api.com/app/data-aeqmn/endpoint/data/v1/action/find"
+  payload = json.dumps({
+      "collection": "bikes",
+      "database": "bike_database",
+      "dataSource": "bike",
+      "projection":{
+        "_id" : 0
+      }
+  })
+  headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': "Xt3Zlv6lt9PaokpzjqYvIhqVD5eD4ild4mVKSczjZ3xzT2WWavTABFAD1u3ng9Sl",
+  }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+  response = requests.request("POST", url, headers=headers, data=payload)
+  result = response.text
+  result = json.loads(result)
 
-print(response.text)
+  f = open("Database/bikes.json", "w", encoding="UTF-8")
+  json.dump(result["documents"], f, ensure_ascii=False)
+  f.close()
+
+ObtenerJson()
