8b50be0581ecace373ab41472e9516079c9219ad
APSalva <117761833+APSalva@users.noreply.github.com>
2022-12-13 17:48:15 +0100

    fix

--- src/Presentacion/ObtenerCarritoVacio.py
+++ src/Presentacion/ObtenerCarritoVacio.py
@@ -22,16 +22,16 @@ def ObtenerCarritoVacio():
     <body>
         <header class="barraNav">
             <div class="logo">
-                <a href="index-Todas.html"><img src="Img/icon.png" weight="80px" height="80x" alt="icono-MaBike"></a>
-            </div>
-            <nav class="barraNav">
-                <ul class="barraNav">
-                    <li><a href="../index.html"><img src="Img/play.png" wight="25px" height="25px" alt="Atrás"></a></li>
-                    <li><a href="../formulario-registro.html">Regístrate <img src="Img/registro.png" wight="25px" height="25px"alt="Registrate"></a></li>
-                    <li><a href="../Carrito-vacio.html">Carrito <img src="Img/carrito.png" wight="25px" height="25px" alt="Carrito"></a></li>
-                    <li><a href="https://www.correos.es/es/es/herramientas/localizador/envios">Envío a Domicilio <img src="Img/envio.png" wight="25px" height="25px" alt="Envio a Domicilio"></a></li>
-                    <li><a href="../localizaciones-palma.html">Recogida en Tienda <img src="Img/tienda.png" wight="25px" height="25px" alt="Recogida en Tienda"></a></li>
-                    <li><a href="https://www.palma.cat/portal/BICIPALMA/RecursosWeb/DOCUMENTOS/9/12_47262_2.pdf">Términos y Condiciones <img src="Img/terminos.png" wight="25px" height="25px" alt="Términos y Condiciones"></a></li>
+                    <a href="index-Todas.html"><img src="Img/icon.png" weight="80px" height="80x" alt="icono-MaBike"></a>
+                </div>
+                <nav class="barraNav">
+                    <ul class="barraNav">
+                        <li><a href="index.html"><img src="Img/play.png" wight="25px" height="25px" alt="Atrás"></a></li>
+                        <li><a href="formulario-registro.html">Regístrate <img src="Img/registro.png" wight="25px" height="25px"alt="Registrate"></a></li>
+                        <li><a href="Carrito-vacio.html">Carrito <img src="Img/carrito.png" wight="25px" height="25px" alt="Carrito"></a></li>
+                        <li><a href="https://www.correos.es/es/es/herramientas/localizador/envios">Envío a Domicilio <img src="Img/envio.png" wight="25px" height="25px" alt="Envio a Domicilio"></a></li>
+                        <li><a href="localizaciones-palma.html">Recogida en Tienda <img src="Img/tienda.png" wight="25px" height="25px" alt="Recogida en Tienda"></a></li>
+                        <li><a href="https://www.palma.cat/portal/BICIPALMA/RecursosWeb/DOCUMENTOS/9/12_47262_2.pdf">Términos y Condiciones <img src="Img/terminos.png" wight="25px" height="25px" alt="Términos y Condiciones"></a></li>
                 </ul>
             </nav>
         </header>
