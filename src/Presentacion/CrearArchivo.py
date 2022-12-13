ffadedc4c785d4cbb6a04863a010a2796c6f28a0
Juan Tur <jtur@cifpfbmoll.eu>
2022-12-13 15:42:50 +0100

    he quitado un comentario

--- src/Presentacion/CrearArchivo.py
+++ src/Presentacion/CrearArchivo.py
@@ -2,17 +2,17 @@
 def CrearArchivo(archivo,contenido):
     
     # precondiciones:
-    # assert isinstance(archivo,str), "No es un string"
-    # assert isinstance(contenido,str), "No es un string"
-    # assert len(archivo) > 0
+    assert isinstance(archivo,str), "No es un string"
+    assert isinstance(contenido,str), "No es un string"
+    assert len(archivo) > 0
 
-    # try: 
+    try: 
         crear_archivo = open(archivo,"w", encoding="UTF-8")
-    # except FileNotFoundError:
-    #     print("La ruta o el nombre del archivo no es correcto")
-    # except TypeError: 
-    #     print("El tipo del nombre del archivo es incorrecto")
-    # except IOError: 
-    #     print("Error de entrada/salida")
-    # else:
+    except FileNotFoundError:
+        print("La ruta o el nombre del archivo no es correcto")
+    except TypeError: 
+        print("El tipo del nombre del archivo es incorrecto")
+    except IOError: 
+        print("Error de entrada/salida")
+    else:
         crear_archivo.write(contenido)
